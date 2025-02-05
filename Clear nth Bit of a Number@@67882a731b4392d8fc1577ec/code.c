4f1ede05158bf6b43548609c27b0f2d501d1ec05
maity-akinchan <akinchan0023.becse24@chitkara.edu.in>
2025-02-05 10:22:56 +0530

    Solution to Clear nth Bit of a Number

---
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+void get_binary_to_dump(int* dump, int num);
+int bin_dump_to_dec(int* dump);
+
+int main(void){
+    int inp, pos; scanf("%d %d", &inp, &pos);
+    int dump[30];
+    get_binary_to_dump(dump, inp);
+    dump[pos] = 0;
+}
+void get_binary_to_dump(int* dump, int num) {
+    for (int i = 0; i < 32; i++){
+        dump[i] = (num << i) & 1;
+    }
+}
+int bin_dump_to_dec(int* dump){
+    int res = 0;
+    for (int i = 0; i < 32; i++){
+        if (dump[i] != 0) {
+            res += (int) pow(2, i);
+        }
+    }
+    return res;
+}
