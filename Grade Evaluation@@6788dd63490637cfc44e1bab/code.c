6a47bd57c06849a31079933d98083ec5712b8887
maity-akinchan <akinchan0023.becse24@chitkara.edu.in>
2025-02-08 22:47:16 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -7,7 +7,7 @@ int main(void) {
         case 'B': printf("Good"); break;
         case 'C': printf("Average"); break;
         case 'D': printf("Below Average"); break;
-        case 'F': printf("Fail") break;
+        case 'F': printf("Fail"); break;
         default: printf("Invalid grade");
     }
 }
