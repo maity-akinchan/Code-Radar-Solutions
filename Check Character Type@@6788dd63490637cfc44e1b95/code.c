a60c12b0136988b887d90294203f14d6b801b507
maity-akinchan <akinchan0023.becse24@chitkara.edu.in>
2025-02-08 13:32:13 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,6 +6,8 @@ int main (void) {
         printf("Vowel");
     else if (!(65 <= x <= 90 || 97 <= x <= 122))
         printf("Special Character");
+    else if (30 <= x <= 39)
+        printf("Digit")
     else 
         printf("Consonant");
 }
