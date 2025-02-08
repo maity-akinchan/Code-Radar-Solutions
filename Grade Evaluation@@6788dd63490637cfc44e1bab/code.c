f23ba579d1d4b6b91a276a524b6108526477ef7c
maity-akinchan <akinchan0023.becse24@chitkara.edu.in>
2025-02-08 22:46:51 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    char inp; scanf("%c", &inp);
+    switch (inp) {
+        case 'A': printf("Excellent"); break;
+        case 'B': printf("Good"); break;
+        case 'C': printf("Average"); break;
+        case 'D': printf("Below Average"); break;
+        case 'F': printf("Fail") break;
+        default: printf("Invalid grade");
+    }
+}
